aca52b80c3df4bcb7ae38e4c4c5a83dd34478f26
nendy01 <nendylapara@gmail.com>
2023-03-09 16:06:36 -0400

    ahora si, lo prometo

--- eciprion.js
+++ eciprion.js
@@ -6,27 +6,32 @@ for (let i = 1; i <= 87; i++) {
   const audioContainer = document.createElement("div")
   const audio = document.createElement("audio")
   const audioTitle = document.createElement("h2")
-    audio.setAttribute("controls","controls")
-    audio.preload = "metadata"
+  audio.setAttribute("controls", "controls")
+  audio.preload = "auto"
+
   if (i < 10) {
-      audio.src = `./audios/GS_Track_0${i}.mp3`
-      audioTitle.textContent = `GS_Track_0${i}`
-  } else {
-      audio.src = `./audios/GS_Track_${i}.mp3`
-     audioTitle.textContent = `GS_Track_${i}`
+    audio.src = i === 1 ? `./audios/GS_Track_01.mp3` : `./audios/GS_Track_0${i}.mp3`
+    audioTitle.textContent = `GS_Track_0${i}`
+  }else {
+    audio.src = `./audios/GS_Track_${i}.mp3`
+    audioTitle.textContent = `GS_Track_${i}`
   }
-
-  if (i === 2) audio.setAttribute("autoplay","autoplay")
+  audio.classList.add(`audio${i}`)
+  if (i == 1) {
+    audio.setAttribute('autoplay', 'autoplay');
+  }
   audio.type = "audio/mp3"
-
-  audioContainer.append(audioTitle,audio)
-
-audio.addEventListener("ended", ({target}) =>{
+  if (i === 1) audioTitle.textContent = `Introduction`
+  audio.setAttribute("id", i)
+  
+  audioContainer.append(audioTitle, audio)
+  
+  audio.addEventListener("ended", ({ target }) => {
     const nextAudio = target.parentElement.nextElementSibling.lastElementChild
     setTimeout(() => nextAudio.play(), 3000);
-})
-
-  fragment.appendChild(audioContainer) 
+  })
+  
+  fragment.appendChild(audioContainer)
 }
 
 document.body.appendChild(fragment)
