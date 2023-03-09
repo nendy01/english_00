8e5c47bf2cf45f8e633e0bd6aa3bf5d8f909ed84
nendy01 <nendylapara@gmail.com>
2023-03-09 12:12:24 -0400

    ahora si

--- eciprion.js
+++ eciprion.js
@@ -7,6 +7,7 @@ for (let i = 1; i <= 87; i++) {
   const audio = document.createElement("audio")
   const audioTitle = document.createElement("h2")
     audio.setAttribute("controls","controls")
+    audio.preload = "metadata"
   if (i < 10) {
       audio.src = `./audios/GS_Track_0${i}.mp3`
       audioTitle.textContent = `GS_Track_0${i}`
